bb5820f9b0a5ccf9d4e075f19e15098f1ca986c9
Pratik Yesane <pratiky2000@gmail.com>
2023-12-27 21:47:00 +0530

    Made some little changes

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -1,14 +1,11 @@
-Issue: #ISSUENUMBER
-
-<!-- Add the issue number that is fixed by this PR (In the form Issue: 123) -->
-<!-- remove these comment lines-->
+Issue: #175
 
 #### Short description of what this resolves:
 
-
-
 #### Changes proposed in this pull request and/or Screenshots of changes:
 
--
--
--
+## 1.Added hover and active state to the buttons and removed the inner shadow and applied it to active state only just a small change
+
+## 2.Fixed the Sidebar,Now rather opening on the side it appear on the front and alinged it,some css thats all
+
+Just did small changes
--- js/navbar.js
+++ js/navbar.js
@@ -1,30 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var navLinks = document.querySelectorAll('.menu a');
+  const toggle = document.querySelector(".toggle");
+  const menu = document.querySelector(".menu");
 
-    function onScroll() {
-        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-        var offset = window.innerHeight * 0.6;
-
-        navLinks.forEach(function (navLink) {
-            var targetDivId = navLink.getAttribute('href').substring(1);
-            var targetDiv = document.getElementById(targetDivId);
-
-            if (targetDiv) {
-                var divTop = targetDiv.offsetTop;
-                var divHeight = targetDiv.offsetHeight;
-
-                if (scrollPosition >= divTop - offset && scrollPosition < divTop + divHeight - offset) {
-                    navLinks.forEach(function (link) {
-                        link.classList.remove('active');
-                    });
-
-                    navLink.classList.add('active');
-                }
-            }
-        });
+  function handleMenuVisibility() {
+    if (window.innerWidth <= 986) {
+      if (toggle.checked) {
+        menu.style.display = "flex";
+      } else {
+        menu.style.display = "none";
+      }
+    } else {
+      menu.style.display = "flex";
     }
+  }
 
-    window.addEventListener('scroll', onScroll);
+  toggle.addEventListener("click", function () {
+    handleMenuVisibility();
+  });
 
-    onScroll();
+  menu.addEventListener("click", function () {
+    if (window.innerWidth <= 986) {
+      menu.style.display = "none";
+      toggle.checked = false;
+    }
+  });
+
+  window.addEventListener("resize", function () {
+    handleMenuVisibility();
+  });
+
+  handleMenuVisibility();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var navLinks = document.querySelectorAll(".menu a");
+
+  function onScroll() {
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    var offset = window.innerHeight * 0.6;
+
+    navLinks.forEach(function (navLink) {
+      var targetDivId = navLink.getAttribute("href").substring(1);
+      var targetDiv = document.getElementById(targetDivId);
+
+      if (targetDiv) {
+        var divTop = targetDiv.offsetTop;
+        var divHeight = targetDiv.offsetHeight;
+
+        if (
+          scrollPosition >= divTop - offset &&
+          scrollPosition < divTop + divHeight - offset
+        ) {
+          navLinks.forEach(function (link) {
+            link.classList.remove("active");
+          });
+
+          navLink.classList.add("active");
+        }
+      }
+    });
+  }
+
+  window.addEventListener("scroll", onScroll);
+
+  onScroll();
 });
