4cfe300b8f843aaf9df88db5addffe10c66eae3a
CodinJack <115005390+CodinJack@users.noreply.github.com>
2024-01-02 23:30:16 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,36 @@ window.addEventListener("scroll", function () {
 moveToTop.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll('section');
+
+  function updateURLHash() {
+    const visibleSection = Array.from(sections).find(section => {
+      const rect = section.getBoundingClientRect();
+      return rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+    });
+
+    if (visibleSection) {
+      const sectionId = visibleSection.id;
+      if (window.location.hash !== `#${sectionId}`) {
+        history.replaceState(null, null, `#${sectionId}`);
+      }
+    }
+  }
+
+  function handleScroll() {
+    updateURLHash();
+    sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      const isVisible = rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+      if (!section.hasAttribute('data-transitioned') && isVisible) {
+        section.classList.toggle('section-visible', true);
+        section.setAttribute('data-transitioned', '');
+      }
+    });
+  }
+  window.addEventListener("scroll", handleScroll);
+  window.addEventListener("resize", handleScroll);
+  updateURLHash();
+  handleScroll();
+});
